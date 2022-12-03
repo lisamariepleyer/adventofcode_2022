f8db29cb8f566ce487bf0dc689628273f4d9267c
Lisa-Marie Pleyer <lisaminusmarie.p@gmail.com>
2022-12-03 12:04:37 +0100

    add solution in python of day 3

---
+++ day3/day3.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+import string
+from itertools import chain
+
+### DAY 3
+
+## PART 1
+
+priority = list(chain.from_iterable([
+    list(string.ascii_lowercase),
+    list(string.ascii_uppercase)
+]))
+
+with open(r"data.txt", "r") as f:
+    valueCommonItems = 0
+
+    for line in f:
+        compartmentOne, compartmentTwo = line[:len(line)//2], line[len(line)//2:]
+        valueCommonItems += priority.index(''.join(set(compartmentOne).intersection(compartmentTwo))) + 1
+
+print(f"The sum of the misplaced item types of all rucksacks is {valueCommonItems}.")
+
+
+## PART 2
+
+groupSize = 3
+
+with open(r"data.txt", "r") as f:
+    allRucksacks = list()
+    valueCommonItems = 0
+
+    for line in f:
+        allRucksacks.append(line.strip())
+
+    allRucksacks = [allRucksacks[i:i+3] for i in range(0, len(allRucksacks), 3)]
+
+    for groupRucksack in allRucksacks:
+        valueCommonItems += priority.index(''.join(set.intersection(*map(set, groupRucksack)))) + 1
+
+print(f"The sum of the item types shared between groups is {valueCommonItems}.")
