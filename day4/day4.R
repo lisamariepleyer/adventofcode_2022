212e0ab5f1156e930e118529164136aea40f36c9
Lisa-Marie Pleyer <lisaminusmarie.p@gmail.com>
2022-12-04 10:10:00 +0100

    add solution in R of day 4

---
+++ day4/day4.R
@@ -0,0 +1,58 @@
+library(data.table)
+
+### DAY 4
+
+## PART 1
+
+data <- fread("data.txt", header = F, col.names = c("elf.one", "elf.two"))
+
+range.endpoints <- function(range) {
+  return(as.integer(strsplit(range, split = "-")[[1]]))
+}
+
+is.contained.in.other <- sapply(1:nrow(data), function(i) {
+  
+  nums <- range.endpoints(as.vector(data[i, elf.one]))
+  e1 <- nums[1]:nums[2]
+  
+  nums <- range.endpoints(as.vector(data[i, elf.two]))
+  e2 <- nums[1]:nums[2]
+  
+  overlap.length <- length(intersect(e1, e2))
+  
+  if(length(e1) == overlap.length || length(e2) == overlap.length) {
+    return(T)
+  } else {
+    return(F)
+  }
+})
+
+cat("In ",
+    sum(is.contained.in.other),
+    " pairs one range is fully contained in the other.\n",
+    sep = "")
+
+
+## PART 2
+
+is.overlapping.other <- sapply(1:nrow(data), function(i) {
+  
+  nums <- range.endpoints(as.vector(data[i, elf.one]))
+  e1 <- nums[1]:nums[2]
+  
+  nums <- range.endpoints(as.vector(data[i, elf.two]))
+  e2 <- nums[1]:nums[2]
+  
+  overlap.length <- length(intersect(e1, e2))
+  
+  if(overlap.length > 0) {
+    return(T)
+  } else {
+    return(F)
+  }
+})
+
+cat("In ",
+    sum(is.overlapping.other),
+    " pairs the two ranges overlap.\n",
+    sep = "")
