c2e01d7681cd8fd8894d30563958c43872317940
Lisa-Marie Pleyer <lisaminusmarie.p@gmail.com>
2022-12-22 14:25:39 +0100

    add solution for day 10 in R

---
+++ day10/day10.R
@@ -0,0 +1,46 @@
+library(data.table)
+
+### DAY 10
+
+## PART 1
+
+input <- readLines("data.txt")
+
+X <- 1
+
+for (cmd in input) {
+  if (cmd == "noop") {
+    X <- c(X, X[length(X)])
+  } else {
+    X <- c(X, X[length(X)])
+    X <- c(X, X[length(X)] + as.integer(strsplit(cmd, split = " ")[[1]][2]))
+  }
+}
+
+cat("The sum of the six signal strengths is ",
+    sum(X[seq(20, 220, 40)] * seq(20, 220, 40)),
+    ".\n",
+    sep = "")
+
+
+## PART 2
+
+cat("The letters are:\n\n")
+
+for (cycle in 1:length(X)) {
+  
+  sprite <- cycle %% 40
+  if (sprite == 0) {
+    sprite <- 40
+  }
+  
+  if(sprite %in% X[cycle]:(X[cycle]+2)) {
+    cat("#")
+  } else {
+    cat(".")
+  }
+  if (cycle %in% seq(0, 240, 40)) {
+    cat("\n")
+  }
+}
+
